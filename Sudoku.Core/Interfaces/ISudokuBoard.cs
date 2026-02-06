559b702a612c433403601156c4d345c022456553
ilanpo <54080770+ilanpo@users.noreply.github.com>
2026-02-06 12:55:29 +0200

    initial project organisation

---
+++ Sudoku.Core/Interfaces/ISudokuBoard.cs
@@ -0,0 +1,31 @@
+﻿using Sudoku.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sudoku.Core.Interfaces
+{
+    public interface ISudokuBoard
+    {
+        int EdgeSize { get; }
+        int BlockSize { get; }
+
+
+        int GetCellValue(int row, int col);
+        bool IsSet(int row, int col);
+        bool IsSolved();
+        bool IsLegalAssignment(int row, int col, int val);
+        List<int> GetCellCandidates(int row, int col);
+
+
+        void SetCellValue(int row, int col, int val);
+        void RemoveCellCandidate(int row, int col, int val);
+
+
+        ISudokuBoard Clone();
+        List<(int Row, int Col)> GetNonAssignedCells();
+        List<(int Row, int Col, int Count)>? GetNonAssignedCellsWithCount();
+    }
+}
