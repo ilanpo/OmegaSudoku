6bbbd144b8bb192c9ad5bd1462c8a52fdd14bc83
ilanpo <54080770+ilanpo@users.noreply.github.com>
2026-02-14 17:15:01 +0200

    Added documentation

--- Sudoku.Core/Interfaces/ISudokuLoader.cs
+++ Sudoku.Core/Interfaces/ISudokuLoader.cs
@@ -6,10 +6,27 @@ using System.Threading.Tasks;
 
 namespace Sudoku.Core.Interfaces
 {
+    /// <summary>
+    /// loads 2d array game boards from file or string
+    /// </summary>
     public interface ISudokuLoader
     {
+        /// <summary>
+        /// loads single puzzle from source.
+        /// </summary>
+        /// <param name="source">string containing a puzzle or filepath for file containing at least 1 puzzle</param>
+        /// <returns>2d array containing puzzle</returns>
+        /// <exception cref="IOException">thrown if no puzzle found in file</exception>
         int[,] LoadPuzzle(string source);
 
+        /// <summary>
+        /// reads file and returns all valid puzzles found
+        /// </summary>
+        /// <remarks>
+        /// supports two formats: one line puzzle, and grid puzzle. file can contain both mixed.
+        /// </remarks>
+        /// <param name="filePath">the path to the txt file</param>
+        /// <returns>2D arrays representing the loaded puzzles.</returns>
         IEnumerable<int[,]> LoadAllPuzzles(string filePath);
     }
 }
